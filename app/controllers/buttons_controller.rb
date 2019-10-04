6413f0456c8e2d3b3a0b08d183a3bcfc4abb9bee
Hironori Mitanie <sangudadian@gmail.com>
2019-10-04 22:10:08 +0900

    add buttons 1..4

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -1,4 +1,22 @@
 class ButtonsController < ApplicationController
   def share
   end
+
+  def button1
+
+  end
+
+  def button2
+
+  end
+
+  def button3
+
+  end
+
+  def button4
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'buttons#share'
+
+  get '/buttons/button1', to: 'buttons#button1'
+  get '/buttons/button2', to: 'buttons#button2'
+  get '/buttons/button3', to: 'buttons#button3'
+  get '/buttons/button4', to: 'buttons#button4'
 end
